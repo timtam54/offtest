45b35baa4e65fe64cb8626ae0fe1a717533786e5
timhams <timtam54@hotmail.com>
2025-03-02 13:11:49 +1000

    .

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const withPWA = require('next-pwa')({
     dest: 'public', // where the service worker is generated
     register: true, // register the service worker
     skipWaiting: true, // activate new service workers immediately
+    fallbacks: {
+      // Failed page requests fallback to this.
+      document: "/",
+    }
   });
   
   module.exports = withPWA({
